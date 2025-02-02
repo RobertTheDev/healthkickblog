50484a48f9cfb497939ad539905307f413198f7c
Robert Hawker <robertthedev@gmail.com>
2025-02-02 20:22:18 +0000

    feat: added image to sign in page

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import ForgotPasswordForm from './components/ForgotPasswordForm';
 
 export const metadata: Metadata = {
   title: 'Sign In',
@@ -6,8 +7,16 @@ export const metadata: Metadata = {
 
 export default function SignIn() {
   return (
-    <main>
-      <h1>Sign In</h1>
+    <main className="flex min-h-screen justify-center bg-gray-50">
+      <div className="h-screen w-1/2">
+        <img
+          className="h-full w-full object-cover"
+          src="https://assets.bonappetit.com/photos/6362d382b83f24ee8287b0fa/3:4/width_1600/Bon-Appetit-Id-portal-Image.png"
+        />
+      </div>
+      <div className="w-1/2">
+        <ForgotPasswordForm />
+      </div>
     </main>
   );
 }
