0802183209ee8d19922c332c3e54cba7a348892e
Robert Hawker <robertthedev@gmail.com>
2025-02-02 20:21:55 +0000

    feat: created recipes data

---
+++ src/app/data/recipes.ts
@@ -0,0 +1,30 @@
+import Recipe from '../interfaces/Recipe';
+
+const recipes: Recipe[] = [
+  {
+    id: '1',
+    name: 'Super Citrusy Oatmeal',
+    image:
+      'https://assets.bonappetit.com/photos/6792a8775e23b55d62e96f0d/3:4/w_640,c_limit/fgfp-super-citrusy-oatmeal_RECIPE_V2_012125_0066_VOG_final.jpg',
+  },
+  {
+    id: '2',
+    name: 'Creamy Chicken and Broccoli Soup',
+    image:
+      'https://assets.bonappetit.com/photos/6769a4edb3725207a4d7cb3e/3:4/w_640,c_limit/20241101_BA_0225PANTRY_CreamyChickenBroccoliSoup_0269w.jpg',
+  },
+  {
+    id: '23',
+    name: 'Five-Spice Beef Noodle Soup',
+    image:
+      'https://assets.bonappetit.com/photos/67660185f300e79455602b1f/3:4/w_640,c_limit/20241112_02_Five%20Spice%20Beef%20Noodle%20Soup_042.jpg',
+  },
+  {
+    id: '4',
+    name: 'Happy Fridge Salad',
+    image:
+      'https://assets.bonappetit.com/photos/676607d69e8c547046bdfbf0/3:4/w_640,c_limit/20241112_04_Happy%20Fridge%20Salad_Recipe_030_Final.jpg',
+  },
+];
+
+export default recipes;
