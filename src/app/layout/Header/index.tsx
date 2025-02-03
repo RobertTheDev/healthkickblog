34cc7103fd23d2272d1ab8a71ba7ebf09d018e78
Robert Hawker <robertthedev@gmail.com>
2025-02-03 23:30:36 +0000

    feat: added auth controls

--- src/app/layout/Header/index.tsx
+++ src/app/layout/Header/index.tsx
@@ -1,60 +1,71 @@
+import { categories } from '@/app/data/categories';
+import { LucideCheckCircle, LucideUser, LucideUserPlus } from 'lucide-react';
 import Link from 'next/link';
-
-const headerLinks = [
-  {
-    id: '1',
-    slug: '/',
-    name: 'Home',
-  },
-  {
-    id: '2',
-    slug: '/',
-    name: 'Home',
-  },
-  {
-    id: '3',
-    slug: '/',
-    name: 'Home',
-  },
-  {
-    id: '4',
-    slug: '/',
-    name: 'Home',
-  },
-];
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-10 h-16 bg-[#173C24]">
-      <div className="flex min-h-full items-center justify-between px-8">
-        <div className="flex items-center gap-8">
-          <div>
-            <Link className="text-xl font-semibold text-white" href="/">
-              Healthkick
+    <header className="sticky top-0 z-[999] flex h-20 items-center justify-between bg-[#173C24] px-8 shadow-md">
+      {/* Logo and Navigation */}
+      <div className="flex items-center justify-between gap-8">
+        {/* Desktop Navigation */}
+        <nav className="hidden gap-12 md:flex">
+          {categories.map((link) => (
+            <Link
+              key={link.id}
+              className="text-base font-medium text-white transition-colors hover:text-orange-400"
+              href={`/categories/${link.slug}`}
+            >
+              {link.name}
             </Link>
-          </div>
-          {headerLinks.map((link) => {
-            return (
-              <div key={link.id}>
-                <Link
-                  className="text-sm font-medium text-white"
-                  href={link.slug}
-                >
-                  {link.name}
-                </Link>
-              </div>
-            );
-          })}
-        </div>
-        <div className="flex gap-4">
-          <Link href="/sign-in" className="rounded-full bg-orange-500 p-4">
-            Sign In
-          </Link>
-          <Link href="/subscribe" className="rounded-full bg-orange-500 p-4">
-            Subscribe
-          </Link>
-        </div>
+          ))}
+        </nav>
       </div>
+
+      {/* Logo */}
+      <div className="flex flex-1 items-center justify-center">
+        <Link
+          href="/"
+          className="space flex items-center gap-2 text-center text-2xl font-bold tracking-wider text-white"
+        >
+          <img className="size-8" src="/logo.svg" />
+          healthkick
+        </Link>
+      </div>
+
+      {/* Action Buttons */}
+      <div className="flex gap-4">
+        <Link href="/profile">
+          <button className="flex items-center gap-2 rounded-full bg-[#2F503B] px-6 py-3 text-sm font-semibold text-white shadow-md hover:bg-[#1A512D]">
+            <LucideUser size={20} />
+            Account
+          </button>
+        </Link>
+        <Link href="/sign-in">
+          <button className="flex items-center gap-2 rounded-full bg-[#2F503B] px-6 py-3 text-sm font-semibold text-white shadow-md hover:bg-[#1A512D]">
+            <LucideUserPlus size={20} /> Sign In
+          </button>
+        </Link>
+        <Link href="/subscribe">
+          <button className="flex items-center gap-2 rounded-full bg-orange-500 px-6 py-3 text-sm font-semibold text-white shadow-md hover:bg-orange-600">
+            <LucideCheckCircle size={20} /> Subscribe
+          </button>
+        </Link>
+      </div>
+
+      {/* Mobile Navigation */}
+      <nav className="bg-[#173C24] px-6 py-3 md:hidden">
+        <div className="flex flex-wrap gap-4">
+          {categories.map((link) => (
+            <Link
+              key={link.id}
+              className="block text-sm font-medium text-white transition-colors hover:text-orange-400"
+              href={`/recipes/${link.slug}`}
+            >
+              {link.name}
+            </Link>
+          ))}
+        </div>
+      </nav>
     </header>
   );
 }
