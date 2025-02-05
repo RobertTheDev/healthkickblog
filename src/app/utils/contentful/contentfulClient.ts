cce588338e15980338302163e5cfd6e062b4a860
Robert Hawker <robertthedev@gmail.com>
2025-02-05 19:21:32 +0000

    chore: set up contentful client

---
+++ src/app/utils/contentful/contentfulClient.ts
@@ -0,0 +1,30 @@
+import { createClient } from 'contentful';
+
+export const contentfulClient = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID!,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
+});
+
+export async function getFoodRecipesByCategory(category: string) {
+  return await contentfulClient.getEntries({
+    content_type: 'foodRecipe',
+    'fields.category': category,
+  });
+}
+
+export async function getFoodRecipesByFeatured() {
+  return await contentfulClient.getEntries({
+    content_type: 'foodRecipe',
+    'fields.featured': true,
+  });
+}
+
+export async function getFoodRecipeBySlug(slug: string) {
+  const response = await contentfulClient.getEntries({
+    content_type: 'foodRecipe',
+    'fields.slug': slug,
+    limit: 1,
+  });
+
+  return response.items[0];
+}
